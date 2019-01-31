042cea837b5191550ed46355b2e540db8c7784f5
mounawarmadani <mounawar.madani@hepl.be>
2019-01-31 11:27:04 +0100

    ex 4 nbr structures complexes dans un fichier

---
+++ markdowns/ex4_nbrstruct.md
@@ -0,0 +1,5 @@
+# Exercice 4
+Ecrire le code de la fonction `get_struct_nbr` pour permettant d'obtenir le nombre de structures présentes dans un fichier.
+
+@[Calcul du nombre d'enregistrements dans un fichier]({"stubs": ["nbrstruct.h"],"command": "sh /project/target/run.sh nbrstruct","project": "nbrstruct"})
+
---
+++ nbrstruct/nbrstruct.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct complex
+{
+	float real;
+	float img;
+};
+int get_struct_nbr(char * nom)
+{
+	//Ecrire le code permettant de renvoyer le nombre d'enregistrements présents dans le fichier dont le nom est contenu dans le paramètre nom
+	// Renvoyer ce nombre ou -1 en cas d'erreur
+	
+}
